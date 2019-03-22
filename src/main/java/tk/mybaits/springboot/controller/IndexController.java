ce05be37547dcb50dfa70195af362eab4397ee54
zhangkun <1797788139@qq.com>
2019-03-22 13:28:39 +0800

    mybaits

---
+++ src/main/java/tk/mybaits/springboot/controller/IndexController.java
@@ -0,0 +1,12 @@
+package tk.mybaits.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/")
+    public String home(){
+        return "hello"+"\n"+" world";
+    }
+}
